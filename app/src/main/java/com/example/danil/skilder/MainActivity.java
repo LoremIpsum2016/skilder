82ff59409aabb8c584c28c0724bf8a489507aeee
Vertaler <dancheran@yandex.ru>
2016-10-24 18:33:28 +0300

    Зона для рисования вынесена в отдельный фрагмент

--- app/src/main/java/com/example/danil/skilder/MainActivity.java
+++ app/src/main/java/com/example/danil/skilder/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.danil.skilder;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DrawFragment.OnFragmentInteractionListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.add(R.id.fragment_container, new DrawFragment());
+        transaction.commit();
+    }
+
+    @Override
+    public void onFragmentInteraction() {
+        //TODO Реализовать правильно
+        Toast.makeText(this, "Fragment interaction", Toast.LENGTH_SHORT).show();
     }
 }
